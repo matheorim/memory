8223ad419d852995c011a5b7bed4efcd6badfb4f
rimbaul251 <matheo.rimbault@etu.unicaen.fr>
2026-09-23 11:15:39 +0200

    feat: core array data logic and fisher-yates shuffle functional

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+let dimension = 50;
+let imgStart = Math.floor(Math.random() * 100);
+
+const urls = [];
+
+for(let i = 0; i < 8; i++) {
+    const url = `https://picsum.photos/id/${imgStart + i}/${dimension}/${dimension}`;
+    urls.push(url);
+}
+
+let cards = [...urls,...urls];
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+cards = shuffle(cards);
+console.log(urls);
+console.log(cards);
